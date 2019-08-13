48d38208005030b596cfa0567b1bb1eb2c8de12a
karenjli <karenjy.li@gmail.com>
2019-08-13 16:28:25 -0500

    number keys added

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,16 +1,24 @@
-import React from "react";
-
+import React, {Component, useState} from "react";
+//import NumberButton from "./NumberButton";
+import NumberButton from "./NumberButton";
+import {numbers} from "../../../data.js";
 //import any components needed
 
 //Import your array data to from the provided data file
 
 const Numbers = () => {
   // STEP 2 - add the imported data to state
+  const [number, setNumber] = useState ({numbers});
+  console.log(useState());
+
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {numbers.map (number => <NumberButton value = {number} />)}
     </div>
   );
 };
+
+export default Numbers;
--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -10,7 +10,11 @@ const Operators = () => {
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+       it any props needed by the child component
+       numbers.map (number => {prop.numbers === number})
+       */}
     </div>
   );
 };
+
+export default Operators;
